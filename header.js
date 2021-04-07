35ffc01982a595b069c09b3c0dd6f316e7d4b664
michellenesbitt <mn218@nerdymail.com>
2021-04-07 07:34:37 -0400

    mn

---
+++ header.js
@@ -0,0 +1,26 @@
+//
+//======================================================
+//; Title:  header.js
+//; Author: Michelle Nesbitt
+//; Date:   4-9-2021
+//; Modified By: Michelle Nesbitt
+//; Description: My portfolio header (sidebar)
+//;=====================================================
+//*/
+
+document.write('<img class="avatar" src="images/mn.jpg" alt="Profile Picture">' +
+    '<br><br>' +
+    '<h2>MICHELLE NESBITT</h2>' +
+    'FREELANCE WEB DEVELOPER<br><br>' +
+    '<p class="sidepar"><a href="index.html">Intro</a><br>' +
+    '<a href="aboutme.html">About Me</a><br><a href="portfolio.html">Design Portfolio</a><br>' +
+    '<a href="resume.html">Resume</a><br> <a href="skills.html">Skills Summary</a><br>' +
+    '<a href="https://github.com/nerdommicro" target="_blank">GitHub Repos</a><br>' +
+    '<a href="https://www.flickr.com/photos/145705562@N06/?" target="_blank">Product Photographs</a><br>' +
+    '<a href="https://www.linkedin.com/in/michellenesbitt" class="fa fa-linkedin" target="new"></a><br><br>' +
+    'My Business Pages<br><' +
+    'a href="https://www.facebook.com/wallplatecity" class="fa fa-facebook" target="new"></a>' +
+    '<a href="https://www.twitter.com/wallplatecity" class="fa fa-twitter" target="new"></a>' +
+    '<a href="https://www.instagram.com/wallplatecity" class="fa fa-instagram" target="new"></a>' +
+    '<a href="https://www.pinterest.com/wallplatecity" class="fa fa-pinterest" target="new">' +
+    '</a>')
