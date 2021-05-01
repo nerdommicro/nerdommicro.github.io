d6ce3cf2a026f9656ac9a429d02484c5292dfe51
Michelle Nesbitt <mn218@nerdymail.com>
2021-05-01 10:58:04 -0400

    mn

--- header.js
+++ header.js
@@ -13,10 +13,10 @@ document.write('<img class="avatar" src="images/mn.jpg" alt="Profile Picture">' 
     '<h2>MICHELLE NESBITT</h2>' +
     'FREELANCE WEB DEVELOPER<br><br>' +
     '<p class="sidepar"><a href="index.html">Intro</a><br>' +
-    '<a href="aboutme.html">About Me</a><br><a href="portfolio.html">Design Portfolio</a><br>' +
-    '<a href="resume.html">Resume</a><br> <a href="skills.html">Skills Summary</a><br>' +
-    '<a href="https://github.com/nerdommicro" target="_blank">GitHub Repos</a><br>' +
-    '<a href="https://www.flickr.com/photos/145705562@N06/?" target="_blank">Product Photographs</a><br>' +
+    '<a href="aboutme.html">About Me</a><br><a href="portfolio.html" target="new">Software Portfolio</a><br>' +
+    '<a href="resume.html" target="new">Resume</a><br> <a href="skills.html" target="new">My Skills</a><br>' +
+    '<a href="https://github.com/nerdommicro" target="_blank">GitHub</a><br>' +
+    '<a href="https://www.flickr.com/photos/145705562@N06/?" target="new">Photography Portfolio</a><br>' +
     '<a href="https://www.linkedin.com/in/michellenesbitt" class="fa fa-linkedin" target="new"></a><br><br>' +
     'My Business Pages<br><' +
     'a href="https://www.facebook.com/wallplatecity" class="fa fa-facebook" target="new"></a>' +
